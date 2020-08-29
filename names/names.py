133cda7c6e25d39fa53e6d5ce5b2877f915a4460
Paul Lee <paul.lee0627@gmail.com>
2020-08-28 17:14:57 -0700

    moved BST to names for conveinence. names is optimized

--- names/names.py
+++ names/names.py
@@ -2,6 +2,62 @@ import time
 
 start_time = time.time()
 
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value >= self.value:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                right_child = self.right
+                right_child.insert(value)
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                left_child = self.left
+                left_child.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if target == self.value:
+            return True
+        if target > self.value:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+        if target < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+
+    # Return the maximum value found in the tree
+    def get_max(self):
+        current = self
+
+        while current.right is not None:
+            current = current.right
+
+        return current.value
+
+    # Call the function `fn` on the value of each node
+    def for_each(self, fn):
+        fn(self.value)
+
+        if self.left:
+            self.left.for_each(fn)
+        
+        if self.right:
+            self.right.for_each(fn)
+
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
@@ -12,15 +68,21 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+bst = BSTNode("val")
+
 # Replace the nested for loops below with your improvements
 """ for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
             duplicates.append(name_1) """
 
-for x, y in zip(names_1, names_2):
-    if x == y:
-        duplicates.append(x)
+for x in names_1:
+    bst.insert(x)
+
+for y in names_2:
+    if bst.contains(y):
+        duplicates.append(y)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
